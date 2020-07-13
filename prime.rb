61635f1a5ca9423d0cc2b47dab2551d39516922f
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-13 22:39:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+#1. Prime number is divisible ONLY by itself, and 1 (not divisible )
+def prime?(number)
+  if number < 0
+    puts "Be sure to account for negative numbers!"
+    false
+  end
+  if number == 1
+    false
+  end
+  
+  x = 2
+  # while x < number
+    
+  # end 
+end
