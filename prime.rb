483d39f0e3d7b67c78b12a5a01c2c32010a766a5
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-13 22:50:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,10 @@
 def prime?(number)
   i = 0
   x = 2
-  if number <= 0
+  if number < 0
     puts "Be sure to account for negative numbers!"
   elsif
-  
+    
   end
   while x < number
     if num % i == 0
