d1915127fa9c7c7f2371196ac9b6a043415d4ce0
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-13 23:04:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,12 @@ require 'pry'
 
 #1. Prime number is divisible ONLY by itself, and 1 (not divisible )
 #primes always have a remainde
-
-range = (2...6)
-range = range.to_a #[2, 3, 4, 5]
-
 def prime?(number)
   i = 0
   x = 2
+  range = (2...6)
+  range = range.to_a #[2, 3, 4, 5]
+
   if number < 0
      return false
   elsif number == 0 || number == 1
